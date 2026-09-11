ec4655fbfbff6315ca41e1576e86e5a28bdc2b37
Oleksandr <kvaqich@gmail.com>
2026-09-11 16:45:20 +0300

    fix: fix preview session failed test

--- ckanext/tour/views/tour_preview.py
+++ ckanext/tour/views/tour_preview.py
@@ -27,6 +27,8 @@ class TourPreviewView(MethodView):
             "endpoint": endpoint,
             "steps": parse_step_forms(tk.request.form),
         }
+
+        session.permanent = True
         session.modified = True
 
         target = _preview_target(endpoint)
